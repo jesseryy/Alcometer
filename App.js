ebfdd014e3ea9bfec869221e8dc4d36f4b33a1d4
jesseryy <jesse.ryynala@gmail.com>
2023-10-29 20:16:20 +0200

    Theme change

--- App.js
+++ App.js
@@ -1,20 +1,118 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Pressable, ScrollView, Text, TextInput, View } from 'react-native';
+import NumericInput from 'react-native-numeric-input';
+import { RadioButton, Switch } from 'react-native-paper';
+import styles, { BasicStyle, DarkStyle } from './styles/style';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const [style, setStyle] = useState(false)
+  const [weight, setWeight] = useState(0);
+  const [bot, setBot] = useState(0);
+  const [h, setH] = useState(0);
+  const [gender, setGender] = useState('Male');
+  const [result, setResult] = useState(0);
+  
+  const currentStyle = style ? DarkStyle : BasicStyle;
+
+  function calculateResult(){
+
+    const lit = bot * 0.33;
+    const gram = lit * 8 * 4.5;
+    const burn = weight / 10;
+    const gramsl = gram - burn * h;
+
+    if (gender === 'Male') {
+      setResult(gramsl / (weight * 0.7))
+    }
+    else {
+      setResult(gramsl / (weight * 0.6))
+    }
+  }
+
+  function resultcolor(){
+    if(result>= 0.5 && result <1){
+      return{
+        color: '#fbff00'
+      }
+    }
+    if(result>=1){
+      return{
+        color: '#ff0000'
+      }
+    }
+    if(result<0.5){
+      return{
+        color: '#2bff00'
+      }
+    }
+  }
+
+
+
+  return (
+    <ScrollView style={currentStyle.container}>
+      <View style={currentStyle.header}>
+      <Switch
+    value={style}
+    onValueChange={ style => setStyle(style) }
+    />
+        <Text style={currentStyle.title}>
+          Alcometer
+        </Text>
+      </View>
+    <View styles={currentStyle.inputs}>
+      <Text style={currentStyle.text}>Weight</Text>
+    </View>
+    <View>
+      <TextInput 
+      style={currentStyle.textInput}
+      keyboardType='decimal-pad'
+      value={weight.toString()}
+      onChangeText={setWeight}/>
+    </View>
+    <View style={currentStyle.inputs}>
+      <Text style={currentStyle.text}>Bottles</Text>
+      <NumericInput
+        value={bot}
+        onChange={v => setBot(v)}
+        minValue={0}
+        containerStyle={{backgroundColor: currentStyle.containerColor}}
+        rounded/>
+       </View> 
+       <View style={currentStyle.inputs}>
+      <Text style={currentStyle.text}>Hours</Text>
+      <NumericInput  
+      value={h}
+      onChange={v => setH(v)}
+      minValue={0}
+      containerStyle={{backgroundColor: currentStyle.containerColor}}
+      rounded/>
+    </View>
+    <View style={currentStyle.inputs}>
+      <RadioButton.Group value={gender} onValueChange={g => setGender(g)}>
+        <View style={currentStyle.radioButton}>
+        <RadioButton value='Male'
+        style={currentStyle.radioButton}/>
+        <Text style={currentStyle.text}>Male</Text>
+        </View>
+        <View style={currentStyle.radioButton}>
+        <RadioButton value='Female'/>
+        <Text style={currentStyle.text}>Female</Text>
+        </View>
+      </RadioButton.Group>
+      </View>
+      <View style={currentStyle.inputs}>
+      <Text style={[currentStyle.result, resultcolor()]}>{result.toFixed(2)}</Text>
+      </View>
+      <View style={currentStyle.inputs}>
+      <Pressable 
+      style={currentStyle.button}
+      onPress={calculateResult}>
+        <Text style={currentStyle.buttonText}>Calculate</Text>
+      </Pressable>
+      </View>
+    </ScrollView>
+  );
+  }
+
---
+++ styles/style.js
@@ -0,0 +1,158 @@
+import { StyleSheet } from 'react-native';
+
+const BasicStyle = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#ffffff',
+      textColor: '#000000',
+    },
+    header: {
+      marginTop: 30,
+      marginBottom: 15,
+      height: 60,
+      backgroundColor: '#11f8ed',
+      flexDirection: 'row'
+    },
+    title: {
+      fontWeight: 'bold',
+      flex: 1,
+      fontSize: 30,
+      textAlign: 'center',
+      margin: 10,
+      fontWeight: 'bold',
+      marginTop: 10,
+      marginBottom: 5,
+      textDecorationLine: 'underline'
+    },
+    textInput: {
+      fontSize: 15,
+      marginTop: 10,
+      marginBottom: 10,
+      padding: 5,
+      width: 'auto',
+      textAlign: 'center',
+      backgroundColor: '#f0ecec',
+      textDecorationLine: 'underline'
+    },
+    button: {
+      margin: 50,
+      flexDirection: "row",
+      padding: 15,
+      backgroundColor: "#11f8ed",
+      width: 200,
+      borderRadius: 15,
+      borderWidth: 3,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 30,
+      marginBottom: 20,
+    },
+    buttonText: {
+      fontSize: 20,
+      fontWeight: 'bold'
+    },
+    inputs: {
+      marginTop: 20,
+      marginBottom: 20,
+      alignItems: 'center',
+    },
+    radioButton: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    },
+    text: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    textDecorationLine: 'underline',
+    marginBottom: 10,
+    marginTop: 10,
+    alignSelf: 'center'
+    },
+    result: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  
+    },
+  
+    containerColor: '#1bff07',
+});
+
+const DarkStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000000',
+    textColor: '#ffffff',
+  },
+  header: {
+    marginTop: 30,
+    marginBottom: 15,
+    height: 60,
+    backgroundColor: '#11f8ed',
+    flexDirection: 'row'
+  },
+  title: {
+    fontWeight: 'bold',
+    flex: 1,
+    fontSize: 30,
+    textAlign: 'center',
+    margin: 10,
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 5,
+    textDecorationLine: 'underline'
+  },
+  textInput: {
+    fontSize: 15,
+    marginTop: 10,
+    marginBottom: 10,
+    padding: 5,
+    width: 'auto',
+    textAlign: 'center',
+    backgroundColor: '#f0ecec',
+    textDecorationLine: 'underline'
+  },
+  button: {
+    margin: 50,
+    flexDirection: "row",
+    padding: 15,
+    backgroundColor: "#11f8ed",
+    width: 200,
+    borderRadius: 15,
+    borderWidth: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 30,
+    marginBottom: 20,
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  inputs: {
+    marginTop: 20,
+    marginBottom: 20,
+    alignItems: 'center',
+  },
+  radioButton: {
+  alignItems: 'center',
+  flexDirection: 'row',
+  },
+  text: {
+  fontSize: 15,
+  fontWeight: 'bold',
+  textDecorationLine: 'underline',
+  marginBottom: 10,
+  marginTop: 10,
+  color: '#ffffff',
+  alignSelf: 'center'
+  },
+  result: {
+  fontSize: 30,
+  fontWeight: 'bold',
+
+  },
+
+  containerColor: '#1bff07',
+});
+
+  export {BasicStyle, DarkStyle };
